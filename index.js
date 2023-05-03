92ae85ff7595506591e02f4da10b2f59ecf1d54f
Jared <jrb32002@gmail.com>
2023-05-03 01:42:56 -0400

    v0.0.2

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+exports.getHexStr = function (color) {
+  var hex;
+  color = color.toUpperCase();
+  if(color === "RED"){
+    hex = "#FF0000";
+  } else if(color === "GREEN"){
+    hex = "#00FF00";
+  } else if(color === "BLUE"){
+    hex = "#0000FF";
+  } else if(color === "WHITE"){
+    hex = "#FFFFFF";
+  } else if(color === "BLACK"){
+    hex = "#000000";
+  } else if(color === "YELLOW"){
+    hex = "#FFFF00";
+  } else if(color === "CYAN"){
+    hex = "#00FFFF";
+  } else if(color === "MAGENTA"){
+    hex = "#FF00FF";
+  } else if(color === "SILVER"){
+    hex = "#C0C0C0";
+  } else if(color === "GRAY"){
+    hex = "#808080";
+  } else if(color === "MAROON"){
+    hex = "#800000";
+  } else if(color === "OLIVE"){
+    hex = "#808000";
+  } else if(color === "LIME"){
+    hex = "#00FF00";
+  } else if(color === "AQUA"){
+    hex = "#00FFFF";
+  } else if(color === "TEAL"){
+    hex = "#008080";
+  } else if(color === "NAVY"){
+    hex = "#000080";
+  } else if(color === "FUCHSIA"){
+    hex = "#FF00FF";
+  } else if(color === "PURPLE"){
+    hex = "#800080";
+  } else {
+    hex = "#000000";
   }
+  return hex;
+}
